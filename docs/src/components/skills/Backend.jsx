a251006f85638326171651c3eb858330fc548c21
RahulV2478 <117884531+RahulV2478@users.noreply.github.com>
2024-03-26 02:33:44 -0500

    Add files via upload

---
+++ docs/src/components/skills/Backend.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Backend = () => {
+  return (
+    <div className='skills__content'>
+      <h3 className='skills__title'>Skills</h3>
+
+      <div className='skills__box'>
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>React</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>Node Js</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>Firebase</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='skills__group'>
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>Unity/Unreal Engine</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>Firebase</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+
+          <div className='skills__data'>
+            <i className='bx bx-badge-check'></i>
+
+            <div>
+              <h3 className='skills__name'>Xcode/Android Studio</h3>
+              <span className='skills__level'>Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
---
+++ docs/src/components/work/Data.jsx
@@ -0,0 +1,50 @@
+import Work1 from '../../assets/work1.jpg';
+import Work2 from '../../assets/work2.jpg';
+import Work3 from '../../assets/work3.jpg';
+import Work4 from '../../assets/work4.jpg';
+import Work5 from '../../assets/work5.jpg';
+import Work6 from '../../assets/apexai.jpg';
+import Work7 from '../../assets/Huffman.jpg'
+
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work6,
+    title: 'ApexAI',
+    category: 'App',
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: 'Receipt Scanner',
+    category: 'App',
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: 'Construction Chatbot',
+    category: 'Web Design',
+  },
+  {
+    id: 5,
+    image: Work7,
+    title: 'Huffman Compressor',
+    category: 'Data Structures',
+  }
+];
+
+export const projectsNav = [
+  {
+    name: 'all',
+  },
+  {
+    name: 'Data Structures',
+  },
+  {
+    name: 'Web Design',
+  },
+  {
+    name: 'App',
+  }
+];
